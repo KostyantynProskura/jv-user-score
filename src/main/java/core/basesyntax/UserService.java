79d536d9b6c32f482c8f5adcd4de81dd0f2bfd02
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-04 10:31:29 +0200

    2) Commit: - Fixed maths - return can handle any length of UserCode now; - Removed empty lines

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,18 +3,15 @@ package core.basesyntax;
 import core.basesyntax.exception.UserNotFoundException;
 
 public class UserService {
-
     public int getUserScore(String[] records, String email) {
 
         for (String record : records) {
             if (record.substring(0,record.indexOf(":")).equals(email)) {
                 return Integer.parseInt(record.substring(record.indexOf(":") + 1,
-                                                        record.indexOf(":") + 3));
+                                                        record.indexOf(":")
+                                                        + (record.length() - record.indexOf(":"))));
             }
-
         }
-
         throw new UserNotFoundException("User with given email doesn't exist");
-
     }
 }
